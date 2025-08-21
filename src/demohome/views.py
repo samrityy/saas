137b6a28d03690bc87f550c43290361ddc21956e
samrityy <smritymulmi23@gmail.com>
2025-08-21 09:40:12 +0545

    fix: check case for  percentage calculation

--- src/demohome/views.py
+++ src/demohome/views.py
@@ -6,13 +6,19 @@ from traffic.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
     qs=PageVisit.objects.all()
     page_qs=PageVisit.objects.filter(path=request.path)
     my_title="My Page"
+    try:
+        percent=(page_qs.count()*100.0)/qs.count(),
+    except ZeroDivisionError:
+        percent = 0
+
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
+        "percent":percent,
         "total_count": qs,
     }
     html_template="home.html" 
