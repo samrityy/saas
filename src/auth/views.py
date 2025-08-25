04af23f636726e01f5e7c2f6c4a6633460227fcc
samrityy <smritymulmi23@gmail.com>
2025-08-25 16:20:50 +0545

    feat: enhance login view to handle empty username and password fields

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 # Create your views here.
 def login_view(request):
-    username=request.POST["username"]
-    password = request.POST["password"]
-    user=authenticate(request,username=username,password=password)
-    if user is not None:
-        login (request, user)
+    if(request.method == "POST"):
+        username= request.POST.get("username") 
+        password = request.POST.get("password") 
+        if all([username,password]):
+            user=authenticate(request,username=username,password=password)
+            print('user',user)
+            if user is not None:
+                login (request, user)
+                print("login here!")
+                return redirect("/")
     return render(request,"auth/login.html",{})    
 
 # def register_view(request):
