11977389c4c8f8aeba84ec38fc972e7fb1557780
samrityy <smritymulmi23@gmail.com>
2025-08-26 15:11:28 +0545

    feat: implement user registration view with form handling

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+
+from django.contrib.auth import get_user_model
+User=get_user_model()
 # Create your views here.
 def login_view(request):
     if(request.method == "POST"):
@@ -14,5 +17,14 @@ def login_view(request):
                 return redirect("/")
     return render(request,"auth/login.html",{})    
 
-# def register_view(request):
-#     return render(request, "auth/login.html", {})
+def register_view(request):
+    if request.method=="POST":
+        username=request.POST.get("username")or None
+        email= request.POST.get("email") or None
+        password=request.POST.get("password") or None
+        
+        #Django Forms
+        # user_exist_qs=User.objects.filter(username__iexact=username).exist
+        # email_exist_qs=User.objects.filter(email__iexact=email).exist
+        User.objects.create_user(username,email=email,password=password)
+    return render(request, "auth/register.html", {})
